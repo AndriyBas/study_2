d9ddaa3e83ccae69569931ddc01dcffd5d2c15f6
Andriy Bas <avikbascra@gmail.com>
2014-11-23 17:36:45 +0200

    implement more functions

--- Marik/Lab3Scala/src/main/scala/Nodes.scala
+++ Marik/Lab3Scala/src/main/scala/Nodes.scala
@@ -1,25 +1,30 @@
 import com.sun.jmx.snmp.tasks.Task
 
 import scala.collection.immutable.Queue
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 /**
  * Created by andriybas on 11/23/14.
  */
 
 
-class OutConnection(val node: Node,
-                    val p: Double)
+class Out(val node: Node,
+          val p: Double) {
+  override def toString = p + " : " + node.toString
+}
 
 class Node(val name: String,
-           val taskServeTime: Int,
-           var resources: Int
+           val resources: Int,
+           val taskServeTime: Int
             ) {
 
   var currentResources = resources
   // all nodes in the beginning are not buzy
   var lastServeTime = 0
 
-  var outConnections = scala.collection.mutable.ArrayBuffer.empty[OutConnection]
+  val outConnections = scala.collection.mutable.ArrayBuffer.empty[Out]
+  val outPSum = scala.collection.mutable.ArrayBuffer.empty[Double]
 
   val workQueue = scala.collection.mutable.Queue.empty[Task]
 
@@ -27,7 +32,14 @@ class Node(val name: String,
 
   // statistic
   var totalServeTasks = 0
-  var coreLoad = scala.collection.mutable.ArrayBuffer.fill(resources + 1)(0) // array of the length of resources, showing total time the resources were loaded (0 - none resource worked,  1 - one resource was busy, 2 - two ....
+  val coreLoad = scala.collection.mutable.ArrayBuffer.fill(resources + 1)(0) // array of the length of resources, showing total time the resources were loaded (0 - none resource worked,  1 - one resource was busy, 2 - two ....
+
+  val rand = new Random()
+
+  def addOut(out: Out) = {
+    outConnections += out
+    outPSum += (outPSum.lastOption.getOrElse(0.0) + out.p)
+  }
 
   def accept(task: Task, time: Int) = {
     if (!hasResources) {
@@ -36,6 +48,7 @@ class Node(val name: String,
     } else {
       workQueue += task
       Simulator.acceptEvent(new Event(this, time + taskServeTime))
+      updateLastSearchTime(time)
       takeResource()
     }
   }
@@ -43,55 +56,142 @@ class Node(val name: String,
   private def hasResources() = currentResources > 0
 
   private def freeResource() = {
-    resources += 1
+    currentResources += 1
   }
 
   private def takeResource() = {
-    resources -= 1
+    currentResources -= 1
   }
 
-  def calculateStatisticOnFinishTask(time: Int) = {
+  private def updateLastSearchTime(time: Int): Unit = {
+
     val numOfResBusy = resources - currentResources
-    totalServeTasks += 1
 
     coreLoad(numOfResBusy) += (time - lastServeTime)
 
+    lastServeTime = time
+  }
+
+  private def calculateStatisticOnFinishTask(time: Int) = {
+
+
+    totalServeTasks += 1
+
+  }
+
+  def getNextRandOut(): Int = {
+    val randP = rand.nextDouble() + 0.001 // add additional value to go below 1.0 a little
+
+    def loop(ax: List[Double], pos: Int): Int = {
+      if (ax.isEmpty) pos - 1
+      else if (ax.head < randP) loop(ax.tail, pos + 1) else pos
+    }
+
+    val p = loop(outPSum.toList, 0)
+    p
+
+    //    def loop(ps: List[Double], pos: Int): Int = ps match {
+    //      case a0 :: Nil => pos
+    //      case a1 :: b1 :: ps1 => if (b1 < randP) pos else loop(b1 :: ps1, pos + 1)
+    //    }
+
+    //    loop(outPSum.toList, 0)
+
+    //    def getPos(pos: Int): Int =
+    //      if (pos == outPSum.size - 1)
+    //        pos
+    //      else {
+    //        if (outPSum(pos + 1) > randP) pos
+    //        else getPos(pos + 1)
+    //      }
+  }
+
+  def passTaskForward(task: Task, time: Int) = {
+
+    val pos = getNextRandOut()
+
+    val o = outConnections(pos)
+
+    o.node.accept(task, time)
 
   }
 
   def serve(time: Int) = {
+
+    val taskToGoForward = scala.collection.mutable.ArrayBuffer.empty[Task]
+
     if (!workQueue.isEmpty) {
       val task = workQueue.dequeue()
+      updateLastSearchTime(time)
       calculateStatisticOnFinishTask(time)
+
       freeResource()
-      lastServeTime = time
+
+      taskToGoForward += task
+      //      passTaskForward(task, time)
     }
 
     // start working with tasks in queue if any and has resources
     while (hasResources() && !waitQueue.isEmpty) {
       accept(waitQueue.dequeue(), time)
     }
+
+    for (tt <- taskToGoForward) {
+      passTaskForward(tt, time)
+    }
+
   }
+
+  def printResults(totalTime: Int) = {
+    println(name)
+    println("Total serve tasks : " + totalServeTasks)
+    for (i <- 1 to resources) {
+      println(i + " : " + (1.0 * coreLoad(i) / totalTime) + " % load")
+    }
+    println("----------------------------------\n")
+
+  }
+
+  override def toString = name + ", cr : " + currentResources + ", lst : " + lastServeTime;
 
 }
 
-class CP2 extends Node("CP", 1, 2)
 
-class NorthBridge extends Node("NorthBridge", 2, 1)
+class CP2 extends Node("CP", 2, 1)
 
-class SouthBridge extends Node("SouthBridge", 2, 1)
+// 0.25e-9
 
-class RAM extends Node("RAM", 2, 1)
+class NorthBridge extends Node("NorthBridge", 1, 20)
 
-class GPU extends Node("GPU", 2, 1)
+// 5e-9
 
-class ISA extends Node("ISA", 2, 1)
+class SouthBridge extends Node("SouthBridge", 1, 40)
 
-class LPT extends Node("LPT", 2, 1)
+// 10e-9
 
-class COM extends Node("COM", 2, 1)
+class RAM extends Node("RAM", 1, 3)
 
-class KMD extends Node("KMD", 2, 1)
+// 0.75e-9
+
+class GPU extends Node("GPU", 1, 4)
+
+// 1e-9
+
+class ISA extends Node("ISA", 1, 10)
+
+// ???
+
+class LPT extends Node("LPT", 1, 200)
+
+// ???
+
+class COM extends Node("COM", 1, 150)
+
+// ???
+
+class KMD extends Node("KMD", 1, 75)
+
+// ???
 
 
 
--- Marik/Lab3Scala/src/main/scala/Simulator.scala
+++ Marik/Lab3Scala/src/main/scala/Simulator.scala
@@ -1,25 +1,139 @@
+import java.awt.Event
+
 /**
  * Created by andriybas on 11/23/14.
  */
 
 class Event(val node: Node,
-            time: Int)
+            val time: Int) {
+  override def toString = "Event[ t : " + time + ", node : " + node.toString
+}
 
-class Task(val id: Int)
+class Task(val id: Int) {
+  override def toString = "task : " + id
+}
 
 
 object Simulator {
 
-  val events = scala.collection.mutable.ListBuffer.empty[Event]
+  var events = List.empty[Event]
+
+  val initialTaskCount = 3
+
+  val simulationTime = 1000
+
+  var currentTime = 0
 
   def acceptEvent(event: Event) = {
 
+    def loop(ax: List[Event], acc: List[Event]): List[Event] = ax match {
+      case Nil => acc.reverse ::: List(event)
+      case x :: ax1 => if (x.time > event.time) acc.reverse ::: event :: ax
+      else loop(ax1, x :: acc)
+    }
+
+    events = loop(events, Nil)
   }
 
+  val cp = new CP2
+  val nb = new NorthBridge
+  val ram = new RAM
+  val gpu = new GPU
+  val sb = new SouthBridge
+  val isa = new ISA
+  val lpt = new LPT
+  val com = new COM
+  val kmd = new KMD
+
+  val nodes = List(cp, nb, ram, gpu, sb, isa, lpt, com, kmd)
+
+  def buildComputer() = {
+
+    // cp
+    cp.addOut(new Out(cp, 0.5))
+    cp.addOut(new Out(nb, 0.5))
+
+    // nb
+    nb.addOut(new Out(cp, 0.5))
+    nb.addOut(new Out(ram, 0.3))
+    nb.addOut(new Out(gpu, 0.15))
+    nb.addOut(new Out(sb, 0.05))
+
+    // ram
+    ram.addOut(new Out(nb, 1.0))
+
+    // gpu
+    gpu.addOut(new Out(nb, 1.0))
+
+    // sb
+    sb.addOut(new Out(kmd, 0.3))
+    sb.addOut(new Out(isa, 0.7))
+
+    // isa
+    isa.addOut(new Out(sb, 0.5))
+    isa.addOut(new Out(lpt, 0.25))
+    isa.addOut(new Out(com, 0.25))
+
+    // kmd
+    kmd.addOut(new Out(sb, 0.5))
+    kmd.addOut(new Out(sb, 0.5)) // out of system - return to cp
+    // TODO : should kmd return to cp ??? ???
+
+    // lpt
+    lpt.addOut(new Out(cp, 1.0)) // out of system - return to cp
+
+    // com
+    com.addOut(new Out(lpt, 0.5))
+    com.addOut(new Out(cp, 0.5)) // out of system - return to cp
+  }
+
+  def initialLoad() = {
+
+    for (i <- 0 until initialTaskCount) {
+      cp.accept(new Task(i), 0)
+    }
+  }
+
+  def runSimulation() = {
+
+    while (!events.isEmpty && events.head.time < simulationTime) {
+      val e = events.head
+
+      events = events.tail
+
+      e.node.serve(e.time)
+    }
+
+  }
+
+  def printResults() = {
+    for (n <- nodes) {
+      n.printResults(simulationTime)
+    }
+
+  }
+
+
+  def testAcceptEvent() = {
+    val cp = new CP2
+    acceptEvent(new Event(cp, 10))
+    acceptEvent(new Event(cp, 1))
+    acceptEvent(new Event(cp, 20))
+    acceptEvent(new Event(cp, 15))
+    acceptEvent(new Event(cp, 5))
+    acceptEvent(new Event(cp, 16))
+    acceptEvent(new Event(cp, 21))
+  }
 }
 
 object Runner {
   def main(args: Array[String]): Unit = {
-    println("lol")
+
+    //    Simulator.testAcceptEvent()
+    Simulator.buildComputer
+    Simulator.initialLoad()
+    Simulator.runSimulation()
+
+    Simulator.printResults()
   }
 }
